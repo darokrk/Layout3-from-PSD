1c6282adc17723bd8aea88f3e853deeb7e046d76
Dariusz Rosiek <daro.krk@interia.pl>
2018-11-01 11:12:17 +0100

    Add :active property, delay scroll when "a" link clicked Add :active property, delay scroll when "a" link was clicked

--- js/script.js
+++ js/script.js
@@ -37,3 +37,23 @@ navBtn.addEventListener('click', function () {
     navBtn.classList.toggle('change');
     navMenu.classList.toggle('change__menu');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('touchstart', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
